2c40a8f7e5656d0ffb2acaea6912d2c07f2bb0af
Yahor <egorikftp@gmail.com>
2025-09-20 22:38:43 +0300

    Add rememberImageFrameBitmap composable for caching ImageFrame bitmaps

--- gif-compose/src/commonMain/kotlin/io/github/composegears/pixelart/gif/compose/ImageFrameCompose.kt
+++ gif-compose/src/commonMain/kotlin/io/github/composegears/pixelart/gif/compose/ImageFrameCompose.kt
@@ -1,6 +1,11 @@
 package io.github.composegears.pixelart.gif.compose
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.ImageBitmap
 import com.shakster.gifkt.ImageFrame
 
 expect fun ImageFrame.toImageBitmap(): ImageBitmap
+
+@Composable
+fun rememberImageFrameBitmap(frame: ImageFrame): ImageBitmap = remember(frame) { frame.toImageBitmap() }
