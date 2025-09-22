01d04ce0e41528e6a64d67b5278f1a75e6c18fdd
Yahor <egorikftp@gmail.com>
2025-09-22 15:49:21 +0300

    adjustments for necessary nav args

--- composeApp/src/commonMain/kotlin/io/github/composegears/pixelart/screen/GifGridSetup.kt
+++ composeApp/src/commonMain/kotlin/io/github/composegears/pixelart/screen/GifGridSetup.kt
@@ -18,20 +18,21 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.window.core.layout.WindowSizeClass.Companion.WIDTH_DP_MEDIUM_LOWER_BOUND
-import com.composegears.tiamat.compose.back
-import com.composegears.tiamat.compose.navArgs
-import com.composegears.tiamat.compose.navController
-import com.composegears.tiamat.compose.navDestination
+import com.composegears.tiamat.compose.*
 import com.shakster.gifkt.GifDecoder
 import com.shakster.gifkt.ImageFrame
 import io.github.composegears.pixelart.core.rememberMutableState
 import io.github.composegears.pixelart.gif.compose.rememberImageFrameBitmap
+import io.github.composegears.pixelart.screen.PixelDrawArgs.GifImportArgs
+import io.github.composegears.pixelart.screen.State.GifGridSetupState
 import io.github.composegears.pixelart.ui.AppHeader
 import io.github.composegears.pixelart.ui.PixelGrid
+import io.github.composegears.pixelart.ui.common.PixelTheme
 import io.github.composegears.pixelart.ui.layout.VerticalSpacer
 import io.github.composegears.pixelart.ui.layout.WeightSpacer
 import io.github.vinceglb.filekit.PlatformFile
@@ -43,108 +44,179 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import net.engawapg.lib.zoomable.rememberZoomState
 import net.engawapg.lib.zoomable.zoomable
+import org.jetbrains.compose.ui.tooling.preview.Preview
 import kotlin.math.roundToInt
+import kotlin.time.Duration
 
 val GifGridSetup by navDestination<PlatformFile> {
     val navController = navController()
     val file = navArgs()
 
-    val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
-
     val viewModel = viewModel { GifGridSetupViewModel(file) }
-    val images by viewModel.frames.collectAsState()
+    val state by viewModel.state.collectAsState()
 
-    if (images.isNotEmpty()) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            var selectedFrame by rememberMutableState(images) { images.first() }
-            var pixelSize by rememberMutableState { 8f }
+    when (val state = state) {
+        is GifGridSetupState -> GifGridSetupUI(
+            state = state,
+            onBack = navController::back,
+            openDrawing = {
+                navController.replace(
+                    entry = PixelDrawingScreen,
+                    navArgs = it
+                )
+            }
+        )
+        is State.None -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+    }
+}
 
-            if (windowSizeClass.isWidthAtLeastBreakpoint(WIDTH_DP_MEDIUM_LOWER_BOUND)) {
-                Row(modifier = Modifier.fillMaxSize()) {
-                    VerticalFrames(
-                        frames = images,
-                        selectedFrame = selectedFrame,
-                        onSelect = { selectedFrame = it }
+@Composable
+private fun GifGridSetupUI(
+    state: GifGridSetupState,
+    onBack: () -> Unit,
+    openDrawing: (GifImportArgs) -> Unit
+) {
+    val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+    val frames = state.frames
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        var selectedFrame by rememberMutableState(frames) { frames.first() }
+        var pixelSize by rememberMutableState { 8f }
+
+        if (windowSizeClass.isWidthAtLeastBreakpoint(WIDTH_DP_MEDIUM_LOWER_BOUND)) {
+            Row(modifier = Modifier.fillMaxSize()) {
+                VerticalFrames(
+                    frames = frames,
+                    selectedFrame = selectedFrame,
+                    onSelect = { selectedFrame = it }
+                )
+                VerticalDivider()
+                Column {
+                    AppHeader(
+                        title = "GIF pixel size",
+                        onBack = onBack
                     )
-                    VerticalDivider()
-                    Column {
-                        AppHeader(
-                            title = "GIF pixel size",
-                            onBack = navController::back
-                        )
-                        Column(modifier = Modifier.fillMaxSize()) {
-                            WeightSpacer()
-                            Box(modifier = Modifier.align(Alignment.CenterHorizontally)) {
-                                Column(
-                                    modifier = Modifier.padding(16.dp),
-                                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .verticalScroll(rememberScrollState())
+                    ) {
+                        WeightSpacer()
+                        Box(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                            Column(
+                                modifier = Modifier.padding(16.dp),
+                                verticalArrangement = Arrangement.spacedBy(8.dp),
+                            ) {
+                                FrameGrid(
+                                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                                    frame = selectedFrame,
+                                    pixelSize = pixelSize
+                                )
+                                VerticalSpacer(16.dp)
+                                Text(
+                                    modifier = Modifier.padding(horizontal = 4.dp),
+                                    text = "Adjust the slider to find real GIF pixel size",
+                                    overflow = TextOverflow.Ellipsis,
+                                    maxLines = 1
+                                )
+                                SliderSection(
+                                    modifier = Modifier.widthIn(max = 400.dp),
+                                    pixelSize = pixelSize,
+                                    onSizeChange = { pixelSize = it }
+                                )
+                                Button(
+                                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                                    onClick = {
+                                        openDrawing(
+                                            GifImportArgs(
+                                                pixelSize = pixelSize.roundToInt(),
+                                                width = state.width,
+                                                height = state.height,
+                                                frames = frames
+                                            )
+                                        )
+                                    }
                                 ) {
-                                    FrameGrid(
-                                        modifier = Modifier.align(Alignment.CenterHorizontally),
-                                        frame = selectedFrame,
-                                        pixelSize = pixelSize
-                                    )
-                                    VerticalSpacer(16.dp)
                                     Text(
-                                        modifier = Modifier.padding(horizontal = 4.dp),
-                                        text = "Adjust the slider to find real GIF pixel size",
-                                        overflow = TextOverflow.Ellipsis,
-                                        maxLines = 1
-                                    )
-                                    SliderSection(
-                                        modifier = Modifier.widthIn(max = 400.dp),
-                                        pixelSize = pixelSize,
-                                        onSizeChange = { pixelSize = it }
+                                        text = "Continue",
+                                        maxLines = 1,
+                                        overflow = TextOverflow.Ellipsis
                                     )
                                 }
                             }
-                            WeightSpacer()
                         }
+                        WeightSpacer()
                     }
                 }
-            } else {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .verticalScroll(rememberScrollState())
-                ) {
-                    AppHeader(
-                        title = "GIF pixel size",
-                        onBack = navController::back
-                    )
-                    WeightSpacer()
-                    Box(modifier = Modifier.align(Alignment.CenterHorizontally)) {
-                        Column(
-                            modifier = Modifier.padding(16.dp),
-                            verticalArrangement = Arrangement.spacedBy(8.dp),
+            }
+        } else {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState())
+            ) {
+                AppHeader(
+                    title = "GIF pixel size",
+                    onBack = onBack
+                )
+                WeightSpacer()
+                Box(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                    Column(
+                        modifier = Modifier.padding(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                    ) {
+                        FrameGrid(
+                            modifier = Modifier.align(Alignment.CenterHorizontally),
+                            frame = selectedFrame,
+                            pixelSize = pixelSize
+                        )
+                        VerticalSpacer(16.dp)
+                        Text(
+                            modifier = Modifier.padding(horizontal = 4.dp),
+                            text = "Adjust the slider to find real GIF pixel size",
+                            overflow = TextOverflow.Ellipsis,
+                            maxLines = 1
+                        )
+                        SliderSection(
+                            modifier = Modifier.widthIn(max = 400.dp),
+                            pixelSize = pixelSize,
+                            onSizeChange = { pixelSize = it }
+                        )
+                        Button(
+                            modifier = Modifier.align(Alignment.CenterHorizontally),
+                            onClick = {
+                                openDrawing(
+                                    GifImportArgs(
+                                        pixelSize = pixelSize.roundToInt(),
+                                        width = state.width,
+                                        height = state.height,
+                                        frames = frames
+                                    )
+                                )
+                            }
                         ) {
-                            FrameGrid(
-                                modifier = Modifier.align(Alignment.CenterHorizontally),
-                                frame = selectedFrame,
-                                pixelSize = pixelSize
-                            )
-                            VerticalSpacer(16.dp)
                             Text(
-                                modifier = Modifier.padding(horizontal = 4.dp),
-                                text = "Adjust the slider to find real GIF pixel size",
-                                overflow = TextOverflow.Ellipsis,
-                                maxLines = 1
-                            )
-                            SliderSection(
-                                modifier = Modifier.widthIn(max = 400.dp),
-                                pixelSize = pixelSize,
-                                onSizeChange = { pixelSize = it }
+                                text = "Continue",
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis
                             )
                         }
                     }
-                    WeightSpacer()
-                    HorizontalDivider()
-                    HorizontalFrames(
-                        frames = images,
-                        selectedFrame = selectedFrame,
-                        onSelect = { selectedFrame = it }
-                    )
                 }
+                WeightSpacer()
+                HorizontalDivider()
+                HorizontalFrames(
+                    frames = frames,
+                    selectedFrame = selectedFrame,
+                    onSelect = { selectedFrame = it }
+                )
             }
         }
     }
@@ -278,27 +350,73 @@ private fun ImageItem(
             bitmap = rememberImageFrameBitmap(frame),
             contentDescription = null
         )
+        Badge(
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(top = 4.dp, end = 4.dp),
+            containerColor = MaterialTheme.colorScheme.surface,
+        ) {
+            Text(text = "${frame.index + 1}", fontSize = 9.sp)
+        }
     }
+}
+
+@Preview
+@Composable
+private fun GifGridSetupPreview() = PixelTheme {
+    GifGridSetupUI(
+        state = GifGridSetupState(
+            width = 0,
+            height = 0,
+            frames = listOf(
+                ImageFrame(
+                    argb = intArrayOf(0, 0, 0, 0),
+                    width = 1,
+                    height = 1,
+                    duration = Duration.ZERO,
+                    timestamp = Duration.ZERO,
+                    index = 0
+                )
+            )
+        ),
+        onBack = {},
+        openDrawing = {}
+    )
 }
 
 class GifGridSetupViewModel(
     private val file: PlatformFile
 ) : ViewModel() {
 
-    private val _frames = MutableStateFlow(emptyList<ImageFrame>())
-    val frames = _frames.asStateFlow()
+    private val _state = MutableStateFlow<State>(State.None)
+    val state = _state.asStateFlow()
 
     init {
-        extractFrames()
+        readGif()
     }
 
-    private fun extractFrames() {
+    private fun readGif() {
         viewModelScope.launch(Dispatchers.Default) {
             val decoder = GifDecoder(file.readBytes())
-
             decoder.use {
-                _frames.update { decoder.asList() }
+                _state.update {
+                    GifGridSetupState(
+                        width = decoder.width,
+                        height = decoder.height,
+                        frames = decoder.asList()
+                    )
+                }
             }
         }
     }
+}
+
+sealed interface State {
+    data object None : State
+
+    data class GifGridSetupState(
+        val width: Int,
+        val height: Int,
+        val frames: List<ImageFrame>
+    ) : State
 }
--- composeApp/src/commonMain/kotlin/io/github/composegears/pixelart/screen/GridSetupScreen.kt
+++ composeApp/src/commonMain/kotlin/io/github/composegears/pixelart/screen/GridSetupScreen.kt
@@ -1,8 +1,13 @@
 package io.github.composegears.pixelart.screen
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.OutlinedButton
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
@@ -10,11 +15,11 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import com.composegears.tiamat.compose.TiamatPreview
-import com.composegears.tiamat.compose.navController
-import com.composegears.tiamat.compose.navDestination
-import com.composegears.tiamat.compose.replace
+import com.composegears.tiamat.compose.*
+import io.github.composegears.pixelart.screen.PixelDrawArgs.NewProjectArgs
+import io.github.composegears.pixelart.ui.AppHeader
 import io.github.composegears.pixelart.ui.common.PixelTheme
 import io.github.composegears.pixelart.ui.layout.VerticalSpacer
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -22,67 +27,92 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 val GridSetupScreen by navDestination {
     val navController = navController()
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        AppHeader(title = "New project", onBack = navController::back)
 
-        var width by rememberSaveable { mutableStateOf("24") }
-        var height by rememberSaveable { mutableStateOf("24") }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp)
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            var width by rememberSaveable { mutableStateOf("24") }
+            var height by rememberSaveable { mutableStateOf("24") }
+            var pixelSize by rememberSaveable { mutableStateOf("1") }
 
-        val enabled by remember {
-            derivedStateOf {
-                width.isNotEmpty() && height.isNotEmpty()
+            val enabled by remember {
+                derivedStateOf {
+                    width.isNotEmpty() && height.isNotEmpty() && pixelSize.isNotEmpty()
+                }
             }
-        }
 
-        OutlinedTextField(
-            value = width,
-            label = {
-                Text(text = "Width")
-            },
-            onValueChange = { input ->
-                if (input.isValidInput()) {
-                    width = input
-                }
-            },
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-        )
-        OutlinedTextField(
-            value = height,
-            label = {
-                Text(text = "Height")
-            },
-            singleLine = true,
-            onValueChange = { input ->
-                if (input.isValidInput()) {
-                    height = input
-                }
-            },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-        )
-        VerticalSpacer(8.dp)
-        OutlinedButton(
-            modifier = Modifier,
-            enabled = enabled,
-            onClick = {
-                navController.replace(
-                    entry = PixelDrawingScreen,
-                    navArgs = GridSizeArgs(
-                        width = width.toInt(),
-                        height = height.toInt()
+            OutlinedTextField(
+                value = width,
+                label = {
+                    Text(text = "Width")
+                },
+                onValueChange = { input ->
+                    if (input.isValidInput()) {
+                        width = input
+                    }
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            )
+            OutlinedTextField(
+                value = height,
+                label = {
+                    Text(text = "Height")
+                },
+                singleLine = true,
+                onValueChange = { input ->
+                    if (input.isValidInput()) {
+                        height = input
+                    }
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            OutlinedTextField(
+                value = pixelSize,
+                label = {
+                    Text(text = "Pixel size")
+                },
+                singleLine = true,
+                onValueChange = { input ->
+                    if (input.isValidInput()) {
+                        pixelSize = input
+                    }
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            VerticalSpacer(8.dp)
+            Button(
+                modifier = Modifier,
+                enabled = enabled,
+                onClick = {
+                    navController.replace(
+                        entry = PixelDrawingScreen,
+                        navArgs = NewProjectArgs(
+                            pixelSize = pixelSize.toInt(),
+                            width = width.toInt(),
+                            height = height.toInt()
+                        )
                     )
+                }
+            ) {
+                Text(
+                    text = "Generate PixelGrid",
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
                 )
             }
-        ) {
-            Text("Generate PixelGrid")
         }
     }
 }
 
-private fun String.isValidInput(max: Int = 2048) = all { it.isDigit() } && toInt() <= max
+private fun String.isValidInput(max: Int = 2048) = isEmpty() || all { it.isDigit() } && toInt() <= max
 
 @Preview
 @Composable
